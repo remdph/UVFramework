8bdbc2294e624a6e5b435bbea15d097052453701
Rafael Maldonado <rmaldonado@bancocci.hn>
2017-05-20 15:03:43 -0600

    Completando Framework

--- src/uvframework/UVF.java
+++ src/uvframework/UVF.java
@@ -19,7 +19,7 @@ public class UVF extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("views/FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("views/LoginView.fxml"));
         
         Scene scene = new Scene(root);
         
